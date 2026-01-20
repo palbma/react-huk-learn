8935c22e4852dad9c9e86ebb093b4361b091e195
Олександр Должиков <palbma>
2026-01-20 14:28:18 +0200

    qsqs

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,18 @@
 import './App.css'
-import React, {useState,useEffect,useRef} from 'react'
+import React, {useState,useEffect} from 'react'
+import { Github } from './Github'
 
 
 
 function App() {
 
-    const [firstPlayerCount, setFirstPlayerCount] = useState(10);
-    const [secondPlayerCount, setScondPlayerCount] = useState(10);
-
-    const [count, setCount] = useState({
-        c1:10,
-        c2:20,
-    });
 
   return (
     <>
 
       <div>
             <div className="App">
-                <div>Player  {firstPlayerCount}</div>
-                <button onClick={() => setFirstPlayerCount((actual) => actual +1)}>+1</button>
-                <button onClick={() => setFirstPlayerCount((actual) => actual -1)}>-1</button>
-                <div>Player  {secondPlayerCount}</div>
-                <button onClick={() => setScondPlayerCount((actual) => actual +1)}>+2</button>
-                <button onClick={() => setScondPlayerCount((actual) => actual -1)}>-2</button>
-                <div>idiot {count.c1}</div>
-                <button onClick={() => setCount((actual) => {
-                       return{
-                           ...actual, c1: actual.c1 +1,
-                       }
-                    }
-                    )}>aqs</button>
-                <div>
-                    <div>
-                        <button onClick={() => setScondPlayerCount(10)}>reset second</button>
-                        <button onClick={() => setFirstPlayerCount(10)}>reset first</button>
-                    </div>
-                </div>
+                <Github />
             </div>
       </div>
 
